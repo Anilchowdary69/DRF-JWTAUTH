64a6f08e13384a3c76ee1555afcf1f0b85b9e794
Anilchowdary69 <anilpoka69@gmail.com>
2022-02-19 13:29:14 +0530

    commit

--- jwtauthloginandregister/jwtauthloginandregister/settings.py
+++ jwtauthloginandregister/jwtauthloginandregister/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -128,3 +128,5 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
      ],
 }
+
+django_heroku.settings(locals())
